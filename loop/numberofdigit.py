42c84ccd0d5fc6cb434aede3abc75616557f4eed
Ved Prakash <dubeyved73@gmail.com>
2025-08-20 23:25:44 +0530

    Add files via upload

---
+++ loop/numberofdigit.py
@@ -0,0 +1,3 @@
+num = int(input("Enter a number: "))
+digit_count = len(str(abs(num)))
+print("Number of digits:", digit_count)
---
+++ loop/power.py
@@ -0,0 +1,7 @@
+m = int(input("Enter the number: "))
+n = int(input("Enter the number "))
+result = 1
+for _ in range(n):
+    result =result*m
+
+print("the answer is:",result)
