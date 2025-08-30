0ef51a88e28511ecdb534663628600d81b4a5d02
Ved Prakash <dubeyved73@gmail.com>
2025-08-30 16:17:35 +0530

    Add files via upload

---
+++ Pattern/Rev_HolloPyramid.py
@@ -0,0 +1,10 @@
+n= int(input("enter number "))
+for i in range(1, n + 1):
+    for j in range( n - i + 1):
+        print(" *", end="")
+    for j in range(2*i -1):
+            print(" ", end="")
+    for j in range( n - i + 1):
+            print(" *", end="")
+    print()
+            
---
+++ Pattern/square_star.py
@@ -0,0 +1,6 @@
+n=int(input("enter number:"))
+m=int(input("enter number:"))
+for i in range(0, n):
+    for j in range(0, m):
+        print("*", end=" ")
+    print()  # This will print a newline after the stars
